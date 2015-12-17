74e40f6c8dfb8f29afe79222bac78ee3be55c386
woo <woo88@kaist.ac.kr>
2015-12-17 14:46:34 +0900

    update for my-fashionbook.html

--- save_image.php
+++ save_image.php
@@ -1,7 +1,7 @@
 <?php
   // requires php5
   define('UPLOAD_DIR', '/home/woo/dev/kse523/web/html/data/images/fashionbooks/');
-  $img = $_POST['imgBase64'];
+  $img = $_GET['imgBase64'];
   $img = str_replace('data:image/png;base64,', '', $img);
   $img = str_replace(' ', '+', $img);
   $data = base64_decode($img);
