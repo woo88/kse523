7fbe6e322d1641377838c9dd7209437466c4d6c6
woo <woo88@kaist.ac.kr>
2015-12-17 12:49:49 +0900

    added images for products

--- save_image.php
+++ save_image.php
@@ -1,6 +1,6 @@
 <?php
   // requires php5
-  define('UPLOAD_DIR', '/home/woo/dev/kse523/web/html/data/images/');
+  define('UPLOAD_DIR', '/home/woo/dev/kse523/web/html/data/images/fashionbooks/');
   $img = $_POST['imgBase64'];
   $img = str_replace('data:image/png;base64,', '', $img);
   $img = str_replace(' ', '+', $img);
