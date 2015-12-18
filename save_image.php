66c05d3204207b6a8944d4409be42ee4949f1625
woo <woo88@kaist.ac.kr>
2015-12-18 11:19:10 +0900

    update for my-fashionbook.html

--- save_image.php
+++ save_image.php
@@ -3,21 +3,20 @@
   define('UPLOAD_DIR', 'data/images/fashionbooks/');
   $img = $_POST['imgBase64'];
   $items = $_POST['canvasItems'];
-  echo $items;
   $img = str_replace('data:image/png;base64,', '', $img);
   $img = str_replace(' ', '+', $img);
   $data = base64_decode($img);
   $file = UPLOAD_DIR . uniqid() . '.png';
   $success = file_put_contents($file, $data);
-  // echo $file;
+  echo $file;
   // print $success ? $file : 'Unable to save the file.';
 
-  $result = {"fashionId":uniqid(),"image":$file,"items":$items,"likes":0,"userId":"000001"};
-  $inp = file_get_contents('data/fashionbooks.json');
-  $tempArr = json_encode($inp);
-  array_push($tempArr, $result);
-  $jsonData = json_encode($tempArr);
-  file_put_contents('data/fashionbooks.json', $jsonData);
+  // $result = {"fashionId":uniqid(),"image":$file,"items":$items,"likes":0,"userId":"000001"};
+  // $inp = file_get_contents('data/fashionbooks.json');
+  // $tempArr = json_encode($inp);
+  // array_push($tempArr, $result);
+  // $jsonData = json_encode($tempArr);
+  // file_put_contents('data/fashionbooks.json', $jsonData);
 
   // echo $tempArr;
 ?>
