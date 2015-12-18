d032b71c89f496020267acc3992449686c95744d
woo <woo88@kaist.ac.kr>
2015-12-18 12:04:10 +0900

    update for my-fashionbook.html

--- save_image.php
+++ save_image.php
@@ -13,11 +13,14 @@
 
   $result = array('fashionId'=>uniqid(),'image'=>$file,'items'=>$items,'likes'=>0,'userId'=>'000001');
   $inp = file_get_contents('data/fashionbooks.json');
-  $tempArr = json_encode($inp);
+  $tempArr = json_decode($inp);
+  // echo $tempArr;
   array_push($tempArr, $result);
+  // echo json_encode($result);
   $jsonData = json_encode($tempArr);
+  // echo $jsonData;
   file_put_contents('data/fashionbooks.json', $jsonData);
 
   // echo $tempArr;
-  echo 'php done';
+  // echo 'php done';
 ?>
